55d81d1e2fbadad34105378eccb5c67e0bc91cc2
Noah <noahli@mac-pro.local>
2015-04-10 11:02:28 +0800

    noah

--- src/main/java/com/juxinli/oa/webroot/Rest.java
+++ src/main/java/com/juxinli/oa/webroot/Rest.java
@@ -1,5 +1,8 @@
 package com.juxinli.oa.webroot;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
 
 /**
  * Created by noahli on 2015/4/9.
@@ -32,6 +36,13 @@ public class Rest {
     @RequestMapping(value = "/main/common", method = RequestMethod.GET)
     @ResponseBody
     public ModelAndView common(HttpServletRequest request, HttpServletResponse response) throws IOException, NoSuchAlgorithmException {
+        //获取登陆用户信息
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        //获取登陆用户的所有权限
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+        
         return new ModelAndView("common");
     }
 
